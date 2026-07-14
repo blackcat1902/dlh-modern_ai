0d37891fd88c017f254aa7b0a5e2a0c7c90a91b8
blackcat1902 <fatmacankutuk@gmail.com>
2026-07-14 21:56:53 +0200

    mo

--- data_analysis/data_preparation_visualization/4-remove_duplicates.py
+++ data_analysis/data_preparation_visualization/4-remove_duplicates.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-import pandas as pd
-
 def remove_duplicates(df):
-    # Duplicate rows are removed. 
+# Duplicate rows are removed. 
     df = df.drop_duplicates()
     return df
--- data_analysis/data_preparation_visualization/5-drop_customerID.py
+++ data_analysis/data_preparation_visualization/5-drop_customerID.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import pandas as pd
 def drop_customerID(df):
     df = df.drop(columns=['customerID'])
     return df
