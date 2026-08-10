a1f05b470b49de6c65c265fd100a76b1ef828128
blackcat1902 <fatmacankutuk@gmail.com>
2026-08-10 18:02:52 +0200

    tenserboard

---
+++ deep_learning/dl_with_keras/9-tensorboard.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Observe model performance with
+Tensorboard
+"""
+import datetime
+from tensorflow import keras
+
+
+def log_to_tensorboard(
+        log_dir,
+        model,
+        X, Y, epochs, verbose=1
+):
+    """
+    Train a model while logging
+    TensorBoard data
+    """
+
+    timestamp = datetime.datetime.now().strftime(
+        '%Y%m%d-%H%M%S'
+    )
+    full_log_dir = f{log_dir}/{timestamp}
+
+    tensorboard_callback = keras.callbacks.TensorBoard(
+        log_dir=full_log_dir,
+        histogram_freq=1
+    )
+
+    model.fit(
+        X, Y,
+        epochs=epochs,
+        verbose=verbose,
+        callbacks=[tensorboard_callback]
+    )
+    
